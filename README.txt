46b230bba78d7c050a41fb67fd70da38efe453bf
Moshtaba <mojtabagh76@gmal.com>
2019-01-13 02:54:28 +0330

    full

---
+++ README.txt
@@ -0,0 +1,8 @@
+"Hungry Crows" Project for "Operating Systems".
+Teacher: Dr Atie Hoseinpour
+Student: Mojtaba JafarGholizadeh
+Student Number: 951542181014
+date: january 2019
+https://github.com/Mojtaba-Gh/Hungry-Crows.git
+
+NOTE: run this program in Linix with gcc compiler with "-lpthread". like this: "gcc hungry-crows.c -lpthread"
--- hungry-crows.c
+++ hungry-crows.c
@@ -1,3 +1,12 @@
+// "Hungry Crows" Project for "Operating Systems".
+// Teacher: Dr Atie Hoseinpour
+// Student: Mojtaba JafarGholizadeh
+// Student Number: 951542181014
+// date: january 2019
+// https://github.com/Mojtaba-Gh/Hungry-Crows.git
+
+// NOTE: run this program in Linix with gcc compiler with "-lpthread". like this: "gcc hungry-crows.c -lpthread"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <semaphore.h>
