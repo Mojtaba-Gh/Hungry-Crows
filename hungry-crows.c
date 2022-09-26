7280016eedff2e25ade575ede9fa6b4731ef3f81
Mojtaba JafarGholizadeh <mojtabagh76@gmail.com>
2022-09-26 15:00:21 +0330

    remove stu number

--- hungry-crows.c
+++ hungry-crows.c
@@ -1,7 +1,6 @@
 // "Hungry Crows" Project for "Operating Systems".
 // Teacher: Dr Atie Hoseinpour
 // Student: Mojtaba JafarGholizadeh
-// Student Number: 951542181014
 // date: january 2019
 // https://github.com/Mojtaba-Gh/Hungry-Crows
 
